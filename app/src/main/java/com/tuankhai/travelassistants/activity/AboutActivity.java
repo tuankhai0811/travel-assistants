54673c99c7684c34bb6dd0bfe5fddbe1ec9871b7
tuankhai0811 <tuankhai0811@gmail.com>
2017-11-22 23:03:38 +0700

    add

--- app/src/main/java/com/tuankhai/travelassistants/activity/AboutActivity.java
+++ app/src/main/java/com/tuankhai/travelassistants/activity/AboutActivity.java
@@ -1,9 +1,15 @@
 package com.tuankhai.travelassistants.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.tuankhai.travelassistants.R;
+import com.tuankhai.travelassistants.library.slideractivity.Slider;
+import com.tuankhai.travelassistants.library.slideractivity.model.SliderConfig;
+import com.tuankhai.travelassistants.library.slideractivity.model.SliderPosition;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -11,5 +17,41 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        initToolbar();
+        initSlider();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(getString(R.string.txt_title_activity_about));
+    }
+
+    private void initSlider() {
+        SliderConfig mConfig = new SliderConfig.Builder()
+                .primaryColor(getResources().getColor(R.color.colorPrimary))
+                .secondaryColor(getResources().getColor(R.color.global_black))
+                .position(SliderPosition.LEFT)
+                .sensitivity(1f)
+                .scrimColor(Color.BLACK)
+                .scrimStartAlpha(0.8f)
+                .scrimEndAlpha(0f)
+                .velocityThreshold(2400)
+                .distanceThreshold(0.2f)
+                .edge(true)
+                .edgeSize(0.2f)
+                .build();
+        Slider.attach(this, mConfig);
     }
 }
